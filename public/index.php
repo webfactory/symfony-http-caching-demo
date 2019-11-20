5a955640a0d28c899d77fe7a621b7003980e139d
Matthias Pigulla <mp@webfactory.de>
2019-11-20 09:28:47 +0000

    Set-up the Symfony Cache in the front controller by env var or cookie
    Having an env var "SYMFONY_WEBAPP_CACHE_ENABLED" defined or having the web server
    started with the "SYMFONY_WEBAPP_KERNEL_PARAMETERS_ALLOW_OVERRIDE" env var
    and then setting a "SYMFONY_CACHE" cookie will enable the Symfony HTTPCache.

--- public/index.php
+++ public/index.php
@@ -1,8 +1,22 @@
 <?php
 
 use App\Kernel;
+use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
+
+// When using the built-in server, start it like "SYMFONY_WEBAPP_KERNEL_PARAMETERS_ALLOW_OVERRIDE=1 bin/console server:start 0.0.0.0:8000"
+if ($_SERVER['SYMFONY_WEBAPP_KERNEL_PARAMETERS_ALLOW_OVERRIDE'] ?? $_ENV['SYMFONY_WEBAPP_KERNEL_PARAMETERS_ALLOW_OVERRIDE'] ?? false) {
+    if (isset($_COOKIE['SYMFONY_ENV'])) {
+        $_ENV['APP_ENV'] = $_SERVER['APP_ENV'] = $_COOKIE['SYMFONY_ENV'];
+    }
+    if (isset($_COOKIE['SYMFONY_NODEBUG'])) {
+        $_ENV['APP_DEBUG'] = $_SERVER['APP_DEBUG'] = false;
+    }
+    if (isset($_COOKIE['SYMFONY_CACHE'])) {
+        $_ENV['SYMFONY_WEBAPP_CACHE_ENABLED'] = $_SERVER['SYMFONY_WEBAPP_CACHE_ENABLED'] = true;
+    }
+}
 
 require dirname(__DIR__).'/config/bootstrap.php';
 
@@ -22,6 +36,12 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
+
+if ($_SERVER['SYMFONY_WEBAPP_CACHE_ENABLED'] ?? $_ENV['SYMFONY_WEBAPP_CACHE_ENABLED'] ?? false) {
+    class AppCache extends HttpCache {}
+    $kernel = new AppCache($kernel);
+}
+
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
