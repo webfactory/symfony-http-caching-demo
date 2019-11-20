9e30add8f11f88fea8413fa8e42abe95685414f0
Matthias Pigulla <mp@webfactory.de>
2019-11-20 09:28:47 +0000

    Add a Behat step to assert the Cache-Control "maxAge" value

--- features/date_time.feature
+++ features/date_time.feature
@@ -3,3 +3,4 @@ Feature: DateTimeController caching
     Scenario: Caching the response in the user-agent's private cache
         When I am on "/"
         Then the response status code should be 200
+        And the expiration time should be 5 seconds
--- tests/BehatContext/HttpCachingContext.php
+++ tests/BehatContext/HttpCachingContext.php
@@ -6,4 +6,11 @@ use Behat\MinkExtension\Context\RawMinkContext;
 
 class HttpCachingContext extends RawMinkContext
 {
+    /**
+     * @Given /^the expiration time should be (\d+) seconds$/
+     */
+    public function theExpirationTimeShouldAtLeastBeSeconds($seconds)
+    {
+        $this->assertSession()->responseHeaderMatches('Cache-Control', "_\bmax-age=$seconds\b_");
+    }
 }
